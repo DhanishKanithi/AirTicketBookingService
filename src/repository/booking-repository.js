53d579d4e4f8b3ace75fc89fb7f6b6c0a38de881
Dhanish Kanithi <dhanishkanithi@gmail.com>
2024-10-25 21:28:01 +0530

    completed create booking

--- src/repository/booking-repository.js
+++ src/repository/booking-repository.js
@@ -1,3 +1,4 @@
+const { where } = require('sequelize');
 const {Booking} = require('../models/index');
 const { AppError, ValidationError } = require('../utils/errors/index');
 
@@ -22,5 +23,22 @@ class BookingRepository {
         }
     }
 
+    async update(bookingId, data) {
+        try {
+            const booking = await Booking.findByPk(bookingId);
+            if(data.status) {
+                booking.status = data.status;
+            }
+            await booking.save();
+            return booking;
+        } catch (error) {
+            throw new AppError(
+                'RepositoryError', 
+                'Cannot update Booking', 
+                'There was some issue updating the booking. Please try again later',
+                StatusCodes.INTERNAL_SERVER_ERROR
+            );
+        }
+    }
 }
 module.exports = BookingRepository;
